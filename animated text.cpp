02e9fb2d99b51a2e08ff0a702b4421b5edc1d179
Vasiliy Lebedev <vasyl.lebedev@yandex.ru>
2022-04-25 12:11:24 +0300

    Add files via upload

---
+++ animated text.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unistd.h>
+using namespace std;
+int main()
+{
+
+string str = "Shit happens";
+string str1 = " ";
+
+
+
+cout << str;
+
+for(int a = 0; a < 12; a++)
+	{
+		usleep(1000*250);
+		cout << "\b \b" << flush;
+	}
+cout << endl;
+return 0;
+}
