789c9ff310c848233a2712e8a62d6c55dcc0cd94
Vasiliy Lebedev <vasyl.lebedev@yandex.ru>
2022-04-25 12:12:27 +0300

    Update animated text.cpp

--- animated text.cpp
+++ animated text.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 #include <unistd.h>
 using namespace std;
-int main()
-{
+int main() {
 
 string str = "Shit happens";
 string str1 = " ";
 
-
-
 cout << str;
 
-for(int a = 0; a < 12; a++)
-	{
+for(int a = 0; a < 12; a++) {
 		usleep(1000*250);
 		cout << "\b \b" << flush;
 	}
